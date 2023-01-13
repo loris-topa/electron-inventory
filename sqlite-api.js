2af7169dcf9e21bbb0cf81d3d0d6daabc7bf1ae0
loris-topa <35865324+loris-topa@users.noreply.github.com>
2023-01-14 00:09:35 +0100

    Update sqlite-api.js

--- sqlite-api.js
+++ sqlite-api.js
@@ -1,8 +1,23 @@
+connection = null
 api = {
 	init: function () {
-		return new sqlite3.Database("database.sqlite3")
+		connection = new sqlite3.Database("database.sqlite3")
+	},
+	install: function () {
+		connection.run("CREATE TABLE product( id INTEGER NOT NULL PRIMARY KEY, description TEXT, image TEXT, seller_id INTEGER NOT NULL, FOREIGN KEY(seller_id) REFERENCES seller(id))");
+		connection.run("CREATE TABLE seller( id INTEGER NOT NULL PRIMARY KEY, name TEXT, image TEXT)");
+		connection.run("CREATE TABLE invoice( id INTEGER NOT NULL PRIMARY KEY, seller_id INTEGER NOT NULL, image TEXT, FOREIGN KEY(seller_id) REFERENCES seller(id))");
+		connection.run("CREATE TABLE invoice_product( id INTEGER NOT NULL PRIMARY KEY, amount INTEGER NOT NULL, invoice_id INTEGER NOT NULL, product_id INTEGER NOT NULL, FOREIGN KEY(product_id) REFERENCES product(id), FOREIGN KEY(invoice_id) REFERENCES invoice(id))");
+	},
+	create_seller: function ($name) {
+		return connection.run("INSERT INTO seller (name) VALUES (:name)", {$name});
+	},
+	create_product: function ($name) {
+		return connection.run("INSERT INTO product (description) VALUES (:name)", {$name});
+	},
+	create_product: function ($name) {
+		return connection.run("INSERT INTO seller (name) VALUES (:name)", {$name});
 	}
-
 }
 
 export api;
